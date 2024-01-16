743efd0b9c1387cc580e7b264dd93b40fe30b89b
mariocstweb <mariocstmra@gmail.com>
2024-01-16 09:18:25 +0100

    fixed img path by js

--- script.js
+++ script.js
@@ -5,32 +5,32 @@ const info = [
   {
     name: "Wayne Barnett",
     role: "Founder & CEO",
-    img: "./img/wayne-barnett-founder-ceo.jpg",
+    img: "wayne-barnett-founder-ceo.jpg",
   },
   {
     name: "Angela Caroll",
     role: "Chief Editor",
-    img: "./img/angela-caroll-chief-editor.jpg",
+    img: "angela-caroll-chief-editor.jpg",
   },
   {
     name: "Walter Gordon",
     role: "Office Manager",
-    img: "./img/walter-gordon-office-manager.jpg",
+    img: "walter-gordon-office-manager.jpg",
   },
   {
     name: "Angela Lopez",
     role: "Social Media Manager",
-    img: "./img/angela-lopez-social-media-manager.jpg",
+    img: "angela-lopez-social-media-manager.jpg",
   },
   {
     name: "Scott Estrada",
     role: "Developer",
-    img: "./img/scott-estrada-developer.jpg",
+    img: "scott-estrada-developer.jpg",
   },
   {
     name: "Barbara Ramos",
     role: "Graphic Designer",
-    img: "./img/barbara-ramos-graphic-designer.jpg",
+    img: "barbara-ramos-graphic-designer.jpg",
   },
 ];
 
@@ -43,7 +43,7 @@ for (let i = 0; i < info.length; i++) {
   cardsHTML += `
     <div class="col mt-3">
       <div class="card" style="width: 18rem">
-        <img src="${person.img}" class="card-img-top" alt="${person.name}" />
+        <img src="img/${person.img}" class="card-img-top" alt="${person.name}" />
         <div class="card-body">
           <h5 class="card-title">${person.name}</h5>
           <p class="card-text">${person.role}</p>
